cb78c4b10b3c598f44d9c6ae99a8f9556e475477
sadya21 <sadyaminebayero@gmail.com>
2024-03-29 12:37:34 +0100

    mise à jour

---
+++ src/app/side-bar-step/side-bar-step.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar-step',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-bar-step.component.html',
+  styleUrl: './side-bar-step.component.scss'
+})
+export class SideBarStepComponent {
+
+}
